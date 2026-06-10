d9eafd3cdeac7c5b803c42cb83e2cc6943c6a36f
TraceOfHumanity <azalexxx02@gmail.com>
2026-06-11 00:25:28 +0300

    Initial Commit

---
+++ learn-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learn-c
+//
+//  Created by Alex on 11.06.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
