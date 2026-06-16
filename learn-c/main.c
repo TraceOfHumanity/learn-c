87cc2c55c239a427017b3089a455012075fcff5b
TraceOfHumanity <azalexxx02@gmail.com>
2026-06-17 01:02:02 +0300

    2ffd8b8f-b47a-4203-96c7-2550784f59f1

--- learn-c/main.c
+++ learn-c/main.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct s {
-  int arraySize;
-  int array[];
-};
+
+// int main(void) {
+//   int i, sum=0,size=0;
+//   printf("Enter the size of the array:\n");
+//   scanf("%d", &size);
+//   int arr[size];
+//   printf("Enter %d element in the array:", size);
+//   for(i=0;i<size;i++){
+//     scanf("%d", &arr[i]);
+//   }
+//   for(i=0;i<size;i++){
+//     sum += arr[i];
+//   }
+//   printf("The sum of the array is: %d \n", sum);
+//
+//   return 0;
+// }
 
 int main(void) {
-  int desiredSize = 5;
-  struct s *ptr;
+  int i, sum=0, size=0;
+  printf("Enter the size of the array:\n");
+  scanf("%d", &size);
+  printf("Enter %d element in the array:", size);
+  int tmp;
+  for (i = 0; i < size; i++) {
+    scanf("%d", &tmp);
+    sum += tmp;
+  }
+  printf("The sum of the array is: %d \n", sum);
 
-  ptr = malloc(sizeof(struct s) + desiredSize * sizeof(int));
   return 0;
 }
