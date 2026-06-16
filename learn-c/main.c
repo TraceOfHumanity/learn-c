2c873c15f8d449a042b25f0096efe9685676d315
TraceOfHumanity <azalexxx02@gmail.com>
2026-06-17 01:51:47 +0300

    523e857e-2356-4357-bf1a-706d4ea19f09

--- learn-c/main.c
+++ learn-c/main.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-// int main(void) {
-//   int i, sum=0,size=0;
-//   printf("Enter the size of the array:\n");
-//   scanf("%d", &size);
-//   int arr[size];
-//   printf("Enter %d element in the array:", size);
-//   for(i=0;i<size;i++){
-//     scanf("%d", &arr[i]);
-//   }
-//   for(i=0;i<size;i++){
-//     sum += arr[i];
-//   }
-//   printf("The sum of the array is: %d \n", sum);
-//
-//   return 0;
-// }
+struct myArray {
+  int len;
+  int array[];
+};
 
 int main(void) {
-  int i, sum=0, size=0;
-  printf("Enter the size of the array:\n");
-  scanf("%d", &size);
-  printf("Enter %d element in the array:", size);
-  int tmp;
-  for (i = 0; i < size; i++) {
-    scanf("%d", &tmp);
-    sum += tmp;
-  }
-  printf("The sum of the array is: %d \n", sum);
+  struct myArray *m = NULL;
+  int arraySize = 0;
 
+  printf("Enter the size of the flexible array\n");
+  scanf("%d", &arraySize);
+
+  size_t size = sizeof(struct myArray);
+  m = malloc(size + (sizeof(int) * arraySize));
+
+  m->len = arraySize;
+  m->array[0] = 55;
+  m->array[1] = 199;
+
+  printf("array is: %d:%d\n", m->array[0], m->array[1]);
   return 0;
 }
