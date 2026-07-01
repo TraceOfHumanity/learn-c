f9e0e1144a5f4c922cc6647048ff34ad06ada1d4
TraceOfHumanity <azalexxx02@gmail.com>
2026-07-02 01:37:31 +0300

    Change functionality to convert decimal input to binary output in main.c

--- learn-c/main.c
+++ learn-c/main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
-#include <math.h>
 
-int convertBinaryToDecimal(long long n);
+long long convertDecimalToBinary(int n);
 
 int main() {
-    long long n;
-    printf("Enter a binary number: ");
-    scanf("%lld", &n);
-    printf("%lld in binary = %d in decimal", n, convertBinaryToDecimal(n));
-    return 0;
+  int n;
+  long long result = 0;
+
+  printf("Enter a decimal number: ");
+  scanf("%d", &n);
+  result = convertDecimalToBinary(n);
+  printf("%d in decimal = %lld in binary", n, result);
+  return 0;
 }
 
-int convertBinaryToDecimal(long long n) {
-    int decimalNumber = 0, i = 0, remainder;
-    while (n!=0)    {
-        remainder = n%10;
-        n /= 10;
-        decimalNumber += remainder*pow(2,i);
-        ++i;
-    }
-    return decimalNumber;
+long long convertDecimalToBinary(int n) {
+  long long binaryNumber = 0;
+  int remainder, i = 0;
+
+  while (n != 0) {
+    remainder = n % 2;
+    n = n / 2;
+    binaryNumber += remainder * i;
+    i = i * 10;
+  }
+
+  return binaryNumber;
 }
