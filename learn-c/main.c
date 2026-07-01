d3f242c33e3ba9e1d84d2bff5ba025ddef0d30d2
TraceOfHumanity <azalexxx02@gmail.com>
2026-07-02 01:24:59 +0300

    convert binary input to decimal

--- learn-c/main.c
+++ learn-c/main.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-struct myArray {
-  int len;
-  int array[];
-};
+int convertBinaryToDecimal(long long n);
 
-int main(void) {
-  struct myArray *m = NULL;
-  int arraySize = 0;
+int main() {
+    long long n;
+    printf("Enter a binary number: ");
+    scanf("%lld", &n);
+    printf("%lld in binary = %d in decimal", n, convertBinaryToDecimal(n));
+    return 0;
+}
 
-  printf("Enter the size of the flexible array\n");
-  scanf("%d", &arraySize);
-
-  size_t size = sizeof(struct myArray);
-  m = malloc(size + (sizeof(int) * arraySize));
-
-  m->len = arraySize;
-  m->array[0] = 55;
-  m->array[1] = 199;
-
-  printf("array is: %d:%d\n", m->array[0], m->array[1]);
-  return 0;
+int convertBinaryToDecimal(long long n) {
+    int decimalNumber = 0, i = 0, remainder;
+    while (n!=0)    {
+        remainder = n%10;
+        n /= 10;
+        decimalNumber += remainder*pow(2,i);
+        ++i;
+    }
+    return decimalNumber;
 }
