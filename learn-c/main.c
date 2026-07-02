91251b25fe7210083bb4a937aee64c1beeff12d0
TraceOfHumanity <azalexxx02@gmail.com>
2026-07-02 21:21:58 +0300

    swap values of w1 and w2 using a temporary variable

--- learn-c/main.c
+++ learn-c/main.c
@@ -3,11 +3,12 @@
 int main(void) {
   short int w1 = 25;
   short int w2 = 77;
-  short int w3 = 0;
+  short int temp = 0;
 
-  w3 = w1 & w2;
+  temp = w1;
+  w1 = w2;
+  w2 = temp;
 
-  printf("%d\n", w3);
 
   return 0;
 }
