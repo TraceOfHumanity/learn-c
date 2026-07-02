1e47c7d9eb05cc8aa7954dde002b273a3b145ccf
TraceOfHumanity <azalexxx02@gmail.com>
2026-07-02 21:23:19 +0300

    swap values of w1 and w2 using bitwise XOR operation

--- learn-c/main.c
+++ learn-c/main.c
@@ -3,12 +3,10 @@
 int main(void) {
   short int w1 = 25;
   short int w2 = 77;
-  short int temp = 0;
 
-  temp = w1;
-  w1 = w2;
-  w2 = temp;
-
+  w1 = w1 ^ w2;
+  w2 = w1 ^ w2;
+  w1 = w1 ^ w2;
 
   return 0;
 }
