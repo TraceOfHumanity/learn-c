8f0a01ec7de1c2ac8d36f795db88ab66945ecaab
TraceOfHumanity <azalexxx02@gmail.com>
2026-07-10 20:14:11 +0300

    fscanf

--- learn-c/main.c
+++ learn-c/main.c
@@ -2,33 +2,18 @@
 #include <stdlib.h>
 
 int main(void) {
-  FILE *f = NULL;
-  char ch[100];
+    FILE *fp = fopen("./data.txt", "r");
+    char buff[255];
 
-  if ((f = fopen("./foo.c", "r+")) == NULL) {
-    printf("Cannot open the file...");
-    exit(1);
-  }
+    if (fp == NULL) {
+        perror("fopen failed");
+        return 1;
+    }
 
-  for (int i = 0; i < 10; i++) {
-    fprintf(f, "// The count numbers is %d\n", i + 1);
-  }
+    while (fscanf(fp, "%254s", buff) != EOF) {
+        printf("%s ", buff);
+    }
 
-  fclose(f);
-
-  if ((f = fopen("./data.txt", "r+")) == NULL) {
-    printf("Cannot open the file...");
-    exit(1);
-  }
-
-  printf("File content is--\n");
-  printf("\n...print the strings...\n\n");
-
-  while (fgets(ch, 100, f)) {
-    printf("%s", ch);
-  }
-
-  fclose(f);
-
-  return 0;
+    fclose(fp);
+    return 0;
 }
