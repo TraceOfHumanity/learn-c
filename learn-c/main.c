2f3efb9cdd16585c55db229ca8b162a1129b171a
TraceOfHumanity <azalexxx02@gmail.com>
2026-07-11 09:44:45 +0300

    Enhance file handling in main.c: append user input to data.txt and display contents.

--- learn-c/main.c
+++ learn-c/main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX 41
 
 int main(void) {
-    FILE *fp = fopen("./data.txt", "r");
-    char buff[255];
+  FILE *fp = NULL;
+  char words[MAX];
 
-    if (fp == NULL) {
-        perror("fopen failed");
-        return 1;
-    }
+  if ((fp = fopen("./data.txt", "a+")) == NULL) {
+    fprintf(stdout, "cant open file.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    while (fscanf(fp, "%254s", buff) != EOF) {
-        printf("%s ", buff);
-    }
+  puts("Enter words to add to the file; press the #");
+  puts("key at the beginning of a line to terminate.");
+  while ((fscanf(stdin, "%40s", words) == 1) && (words[0] != '#'))
+    fprintf(fp, "%s\n", words);
 
-    fclose(fp);
-    return 0;
+  puts("File contents:");
+  rewind(fp); /* go back to beginning of file */
+  while (fscanf(fp, "%s", words) == 1)
+    puts(words);
+
+  puts("Done!");
+  if (fclose(fp) != 0)
+    fprintf(stderr, "Error closing file\n");
+
+  return 0;
 }
